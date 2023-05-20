d8d197964e36918e737e469aacac18dcb2c553cc
Stellah Silva <2020000834@ifam.edu.br>
2023-05-19 23:47:27 -0400

    Lista 02: Ordenações Bolha,Inserção,Seleção,Quicksort,Mergesort

--- Lista02/mergesort.h
+++ Lista02/mergesort.h
@@ -1,6 +1,6 @@
 #ifndef MERGESORT_H_INCLUDED
 #define MERGESORT_H_INCLUDED
 
-
+void mergesort(void);
 
 #endif // MERGESORT_H_INCLUDED
--- Lista02/quicksort.h
+++ Lista02/quicksort.h
@@ -1,6 +1,6 @@
 #ifndef QUICKSORT_H_INCLUDED
 #define QUICKSORT_H_INCLUDED
 
-
+void quicksort(void);
 
 #endif // QUICKSORT_H_INCLUDED
